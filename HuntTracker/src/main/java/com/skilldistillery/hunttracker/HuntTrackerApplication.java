bf46d61528f3a9f9728a43aaa75d9235727cf4cb
jttweedy11 <87153150+jttweedy11@users.noreply.github.com>
2021-10-08 16:46:32 -0500

    create boot, add JPA dep git push

---
+++ HuntTracker/src/main/java/com/skilldistillery/hunttracker/HuntTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hunttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuntTrackerApplication.class, args);
+	}
+
+}
---
+++ HuntTracker/src/test/java/com/skilldistillery/hunttracker/HuntTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hunttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuntTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
